a043f4e2c2bbc08a0ea2a4a2f06623757b3aff0c
daklo91 <coffeedog91@gmail.com>
2024-04-09 16:04:33 +0200

    Create report page, move handler code to component

--- src/routes/report.jsx
+++ src/routes/report.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { addDoc, collection } from "@firebase/firestore";
+import { firestore } from "../firebase_setup/firebase";
 
-function report() {
-  return <div>report</div>;
+function Report() {
+  const [error, setError] = useState(false);
+  const title = useRef();
+  const description = useRef();
+  const email = useRef();
+  const navigate = useNavigate();
+
+  const submitReport = (e) => {
+    e.preventDefault();
+    const reportObject = {
+      title: title.current.value,
+      description: description.current.value,
+      email: email.current.value,
+      date: new Date().toLocaleString("no-NO"),
+    };
+
+    const ref = collection(firestore, "reports");
+
+    addDoc(ref, reportObject)
+      .then(() => {
+        navigate("/takk");
+      })
+      .catch((err) => {
+        setError(true);
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={submitReport}>
+          <p>Tittel</p>
+          <input type="text" ref={title} required />
+          <p>Beskrivelse</p>
+          <textarea ref={description} required />
+          <p>Email*</p>
+          <input type="email" ref={email} required />
+          <p>
+            <small>
+              *Vi bruker email til å kontakte deg hvis vi trenger hjelp med å
+              forstå saken.
+            </small>
+          </p>
+          <button type="submit">Raporter</button>
+        </form>
+        {error ? (
+          <div style={{ color: "red" }}>Noe gikk galt, prøv igjen!</div>
+        ) : null}
+      </div>
+    </>
+  );
 }
 
-export default report;
+export default Report;
