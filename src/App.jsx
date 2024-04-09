3f13b2ac12a1a07857d9aff5e74f34eef5ab86a3
daklo91 <coffeedog91@gmail.com>
2024-04-09 12:05:54 +0200

    Install and test Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import handleSubmit from "./handles/handlesubmit";
+import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const dataRef = useRef();
+
+  const submithandler = (e) => {
+    e.preventDefault();
+    handleSubmit(dataRef.current.value);
+    dataRef.current.value = "";
+  };
 
   return (
     <>
@@ -28,8 +38,14 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="App">
+        <form onSubmit={submithandler}>
+          <input type="text" ref={dataRef} />
+          <button type="submit">Save</button>
+        </form>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
