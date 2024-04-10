23cedf8e52b91681edbbe30a4e774717274d44d3
daklo91 <coffeedog91@gmail.com>
2024-04-10 18:01:42 +0200

    Update to add and read comments

--- src/routes/reciever/ReportOverview.jsx
+++ src/routes/reciever/ReportOverview.jsx
@@ -1,6 +1,13 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  collection,
+  addDoc,
+} from "firebase/firestore";
 import { firestore } from "../../firebase_setup/firebase";
 
 async function fetchData(docID) {
@@ -12,26 +19,52 @@ async function fetchData(docID) {
   return data;
 }
 
+async function fetchCommentData(docID) {
+  const commentCollection = collection(firestore, "reports", docID, "comments");
+  let commentData = [];
+  const querySnapshot = await getDocs(commentCollection);
+  querySnapshot.forEach((doc) => {
+    commentData.push({ id: doc.id, ...doc.data() });
+  });
+
+  commentData.sort((b, a) => new Date(a.date) - new Date(b.date));
+
+  return commentData;
+}
+
 function ReportOverview() {
   const [reportState, setReportState] = useState("");
+  const [commentState, setCommentState] = useState("");
+  const commentRef = useRef(null);
   let { reportID } = useParams();
 
   useEffect(() => {
     async function callAsyncFunction() {
       let data = await fetchData(reportID);
       setReportState(data);
+      let commentData = await fetchCommentData(reportID);
+      setCommentState(commentData);
     }
     callAsyncFunction();
   }, []);
 
-  const updateDescription = useCallback(async (key, value) => {
+  const updateReport = useCallback(async (key, value) => {
     const docRef = doc(firestore, "reports", reportID);
-    await updateDoc(docRef, {
-      [key]: value,
-    });
+    if (key === "comment") {
+      await addDoc(collection(firestore, "reports", reportID, "comments"), {
+        comment: commentRef.current.value,
+        date: new Date().toLocaleString("no-NO"),
+      });
+      commentRef.current.value = "";
+    } else {
+      await updateDoc(docRef, {
+        [key]: value,
+      });
+    }
     let data = await fetchData(reportID);
     setReportState(data);
-    console.log(reportState);
+    let commentData = await fetchCommentData(reportID);
+    setCommentState(commentData);
   }, []);
 
   return (
@@ -46,7 +79,7 @@ function ReportOverview() {
           <p>{reportState.date}</p>
           <select
             value={reportState.priority}
-            onChange={() => updateDescription("priority", event.target.value)}
+            onChange={() => updateReport("priority", event.target.value)}
           >
             <option value="Lav">Lav</option>
             <option value="Middels">Middels</option>
@@ -54,7 +87,7 @@ function ReportOverview() {
           </select>
           <select
             value={reportState.status}
-            onChange={() => updateDescription("status", event.target.value)}
+            onChange={() => updateReport("status", event.target.value)}
           >
             <option value="Ikke startet">Ikke startet</option>
             <option value="Under Arbeid">Under Arbeid</option>
@@ -62,7 +95,7 @@ function ReportOverview() {
           </select>
           <select
             value={reportState.assigned}
-            onChange={() => updateDescription("assigned", event.target.value)}
+            onChange={() => updateReport("assigned", event.target.value)}
           >
             <option value="Ingen">Ingen</option>
             <option value="Dennis Bjørneset">Dennis Bjørneset</option>
@@ -71,6 +104,19 @@ function ReportOverview() {
             <option calue="Ola Nordmann">Ola Nordmann</option>
             <option calue="Christian">Christian</option>
           </select>
+          <textarea ref={commentRef}></textarea>
+          <button onClick={() => updateReport("comment")}>Kommenter</button>
+          {commentState.length == 0 ? (
+            <div>Loading Comments...</div>
+          ) : (
+            commentState.map((comment) => (
+              <div key={comment.id}>
+                <div>{comment.comment}</div>
+                <div>{comment.date}</div>
+              </div>
+            ))
+          )}
+          <div></div>
         </>
       )}
     </div>
